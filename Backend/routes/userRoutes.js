57b233b24aa5e069a5e712b3bd4b4ba96baa4abb
Sachidanand Pradhan <53556482+sachidanand-pradhan@users.noreply.github.com>
2024-01-11 12:02:21 +0530

    commit

--- Backend/routes/userRoutes.js
+++ Backend/routes/userRoutes.js
@@ -4,5 +4,6 @@ const { registerUser } = require("../controllers/userControllers");
 const router = express.Router();
 
 router.route("/").post(registerUser);
+// router.post("/login", authUser);
 
 module.exports = router;
