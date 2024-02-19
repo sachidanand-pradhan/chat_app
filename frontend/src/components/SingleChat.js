5a592805be747a04f3dab62d989026588c2ea301
Sachidanand Pradhan <53556482+sachidanand-pradhan@users.noreply.github.com>
2024-02-19 12:31:52 +0530

    chat page fixed

--- frontend/src/components/SingleChat.js
+++ frontend/src/components/SingleChat.js
@@ -5,7 +5,6 @@ import { ArrowBackIcon } from "@chakra-ui/icons";
 import { getSender, getSenderFull } from "../config/ChatLogics";
 import ProfileModal from "./UI_component/ProfileModel";
 import UpdateGroupChatModel from "./UI_component/UpdateGroupChatModel";
-import VideoPlayer from "./Video";
 
 const SingleChat = ({ fetchAgain, setFetchAgain }) => {
   const { user, selectedChat, setSelectedChat } = ChatState();
@@ -59,8 +58,7 @@ const SingleChat = ({ fetchAgain, setFetchAgain }) => {
       ) : (
         <Box d="flex" alignItems="center" justifyContent="center" h="100%">
           <Text fontSize="3xl" pb="3" fontFamily="work sans">
-            {/* Click on a User to start chatting */}
-            <VideoPlayer />
+            Click on a User to start chatting
           </Text>
         </Box>
       )}{" "}
